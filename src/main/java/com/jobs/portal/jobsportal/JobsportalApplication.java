2fc5e403d7d77e01054ee9d26b18efe09f795459
Inaam Ishaque <inaam.ishaque@systemsltd.com>
2020-12-03 17:04:15 +0400

    initial commit

---
+++ src/main/java/com/jobs/portal/jobsportal/JobsportalApplication.java
@@ -0,0 +1,13 @@
+package com.jobs.portal.jobsportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobsportalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobs/portal/jobsportal/JobsportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobs.portal.jobsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsportalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
