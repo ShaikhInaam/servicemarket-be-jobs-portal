330406125429bf33b4ce4da4e32aeeb0060adf27
Inaam Ishaque <inaam.ishaque@systemsltd.com>
2020-12-03 20:55:17 +0400

    Basic Auth Added

---
+++ src/main/java/com/jobs/portal/jobsportal/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.jobs.portal.jobsportal.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("user").password("password").roles("USER").build());
+        return manager;
+
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .realmName("Your App");
+    }
+
+
+
+}
---
+++ src/main/java/com/jobs/portal/jobsportal/controller/DefaultController.java
@@ -0,0 +1,21 @@
+package com.jobs.portal.jobsportal.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+
+
+
+    @GetMapping("/")
+    String greetings() {
+        return "Greetings! Jobs Portal Service Market pakistan";
+    }
+
+
+
+
+}
